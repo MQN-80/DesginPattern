2756240da38133ef2abf5cc4b7f4a30c0230dc33
yjw <897801598@qq.com>
2022-12-15 17:57:44 +0800

    增加原型模式

---
+++ DesignPattern/src/main/java/ProtoPattern/mirror.java
@@ -0,0 +1,17 @@
+package ProtoPattern;
+
+public class mirror {
+    String clean;
+
+    public String getClean() {
+        return clean;
+    }
+
+    public void setClean(String clean) {
+        this.clean = clean;
+    }
+
+    public mirror(String clean){
+        this.clean=clean;
+    }
+}
